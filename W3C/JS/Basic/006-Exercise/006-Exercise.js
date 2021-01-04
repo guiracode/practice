356358d31b11c13e328859de1ba532b07030a48e
gcode <dbeast@tuta.io>
2021-01-03 22:10:55 -0500

    Completed Exercise 6

--- W3C/JS/Basic/006-Exercise/006-Exercise.js
+++ W3C/JS/Basic/006-Exercise/006-Exercise.js
@@ -1,3 +1,11 @@
 
+/** Exercise 6:
+ *  6. Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar. 
+*/
 
+function isLeapYear(year) {
+    return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+}
 
+isLeapYear(2020);
+isLeapYear(2021);
