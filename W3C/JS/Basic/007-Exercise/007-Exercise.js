82200c879278c2177f395ae170845e55fed32855
gcode <dbeast@tuta.io>
2021-01-07 00:37:16 -0500

    Completed Exercise 7

---
+++ W3C/JS/Basic/007-Exercise/007-Exercise.js
@@ -0,0 +1,16 @@
+
+/** Exercise 7:
+    Write a JavaScript program to find 1st January is being a Sunday between 2014 and 2050. 
+*/
+
+
+function findFirstSunday(){
+for(let y = 2014; y <= 2050; y++) {
+    let d = new Date(y, 0, 1);
+    if (d.getDay() === 0) {
+    console.log(`January 1st falls on a Sunday in ${y}`)
+    }
+}
+}
+
+findFirstSunday();
